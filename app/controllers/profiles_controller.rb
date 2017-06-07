c832bd974441630fdda696a8c00048564d63b60d
Calvin Woo <wooxx074@umn.edu>
2017-06-07 02:05:22 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
-    
+    @profile = Profile.new
   end
  
 end
