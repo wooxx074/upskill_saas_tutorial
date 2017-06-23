318fff0c296486e47be2abf513cb3cac76193a5f
Calvin Woo <wooxx074@umn.edu>
2017-06-23 11:48:03 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -17,6 +17,12 @@ class ProfilesController < ApplicationController
     render action: :new
   end
  end
+# GET to /users/:user_id/profile/edit
+ def edit
+  @user = User.find ( params[:user_id] )
+  @profile = @user.profile
+
+ end
  
  private
   def profile_params
